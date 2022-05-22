eb6361fe97f25c7ca8f6c1b25d9a6298de6702fa
carlos <carlosacgr@gmail.com>
2022-05-22 14:44:24 -0300

    poc para fixar um gráfico do power bi dentro de um projeto React

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+        
+        {/* <a href='https://app.powerbi.com/view?r=eyJrIjoiNGI0MjNjOTUtNzUyMy00YWFjLThkMjAtM2ZjMzcwYzhjOTMzIiwidCI6IjQxNDhhNmRlLTBkZDEtNGQwNC1hNGM1LTc4ZTM3NGU0ZjZkNiIsImMiOjR9'>
+          Dashboard Power BI Bikecione 
+        </a> */}
+
+        <div>
+          <iframe title="powerbi vendas" width="600" height="373.5" src="https://app.powerbi.com/view?r=eyJrIjoiNGI0MjNjOTUtNzUyMy00YWFjLThkMjAtM2ZjMzcwYzhjOTMzIiwidCI6IjQxNDhhNmRlLTBkZDEtNGQwNC1hNGM1LTc4ZTM3NGU0ZjZkNiIsImMiOjR9&pageName=ReportSection" frameborder="0" allowFullScreen="true"></iframe>
+        </div>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
